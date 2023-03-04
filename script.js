eb7676cf187c59e227a59eabc76d954a527e7c98
saajanrana <123057148+saajanrana@users.noreply.github.com>
2023-03-04 12:15:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here!
+const list = document.getElementById('infi-list');
 
+list.addEventListener('scroll', function() {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    // user has reached the end of the list
+    const newItem1 = document.createElement('li');
+    newItem1.textContent = 'Item ' + (list.children.length + 1);
+    const newItem2 = document.createElement('li');
+    newItem2.textContent = 'Item ' + (list.children.length + 2);
+    list.appendChild(newItem1);
+    list.appendChild(newItem2);
+  }
+});
